d56c072badeb010c7e99a8886c3a3af9d384fd0f
AlexandarMilissov <alex.milissov@gmail.com>
2019-06-02 12:04:39 +0300

    m

--- toster.ino
+++ toster.ino
@@ -1,20 +1,100 @@
+/*
+bool arr[] = {          0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      , 1, 1, 1, 1, 1, 1, 1, 1 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      };
+*/
+/*
+bool arr[] = {          1, 1, 1, 0, 1, 0, 1, 0 
+                      , 0, 1, 0, 0, 0, 1, 0, 0 
+                      , 0, 1, 0, 0, 0, 1, 0, 0 
+                      , 1, 1, 1, 0, 1, 1, 1, 0 
+                      , 1, 0, 0, 0, 1, 0, 0, 0 
+                      , 1, 1, 1, 0, 1, 0, 0, 0 
+                      , 1, 0, 0, 0, 1, 0, 0, 0 
+                      , 1, 1, 1, 0, 1, 1, 1, 0 
+                      };
+*/
+/*
+bool arr[] = {          0, 0 ,0, 0, 0, 1, 1, 0 
+                      , 0, 0, 0, 0, 0, 1, 1, 0 
+                      , 0, 0, 0, 0, 1, 1, 0, 0 
+                      , 0, 0, 0, 0, 1, 1, 0, 0 
+                      , 0, 0, 0, 0, 0, 1, 1, 0 
+                      , 0, 0, 0, 0, 1, 1, 1, 1 
+                      , 1, 0, 0, 1, 1, 1, 1, 1 
+                      , 0, 0, 0, 0, 1, 0, 0, 1 
+                      };
+*/                      
+/*
+bool arr[] = {          0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 1, 1, 0, 0, 1, 1, 0 
+                      , 0, 1, 1, 0, 0, 1, 1, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 1, 1, 1, 1, 0, 0 
+                      , 0, 1, 1, 1, 1, 1, 1, 0 
+                      , 0, 1, 1, 0, 0, 1, 1, 0 
+                      };
+*/                     
+/*
+bool arr[] = {          0, 0, 0, 1, 1, 0, 0, 0 
+                      , 0, 0, 1, 1, 1, 0, 0, 0 
+                      , 0, 0, 1, 1, 1, 0, 0, 0 
+                      , 0, 0, 0, 1, 1, 0, 0, 0 
+                      , 0, 0, 0, 1, 1, 0, 0, 0 
+                      , 0, 0, 0, 1, 1, 0, 0, 0 
+                      , 0, 0, 0, 1, 1,0, 0, 0 
+                      , 0, 0, 0, 1, 1, 0, 0, 0 
+                      };
+*/                      
+/*
+bool arr[] = {          0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      };
+*/
+/*
+bool arr[] = {          0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 1, 1, 1, 1, 0, 0 
+                      , 0, 0, 1, 0, 1, 1, 0, 0 
+                      , 0, 0, 1, 0, 0, 1, 0, 0 
+                      , 0, 0, 1, 0, 0, 1, 0, 0 
+                      , 0, 0, 1, 0, 0, 1, 0, 0 
+                      , 0, 0, 1, 1, 1, 1, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      };
+  */
+  
+bool arr[] = {          0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 1, 0, 0, 1, 0, 0 
+                      , 0, 0, 1, 0, 0, 1, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      , 0, 0, 1, 0, 0, 1, 0, 0 
+                      , 0, 0, 1, 1, 1, 1, 0, 0 
+                      , 0, 0, 0, 0, 0, 0, 0, 0 
+                      };
+                 
+/*1-4*/
+/*7-8*/
+/*3-5*/
 #define ser 2 //input
-#define notOE 3 //output enable
+#define notOE 3 //output enable1
 #define rclk 4 //storage register clock
 #define srclk 5 //shift register clock
-
-#define diodes 64
-int i = diodes;
-
-char arr[diodes] = {1, 1, 1, 1, 1, 1, 1, 1
-                    /*2*/, 1, 1, 1, 1, 1, 1, 1, 1
-                    /*3*/, 1, 1, 1, 1, 1, 1, 1, 1
-                    /*4*/, 1, 1, 1, 1, 1, 1, 1, 1
-                    /*5*/, 1, 1, 1, 1, 1, 1, 1, 1
-                    /*6*/, 1, 1, 1, 1, 1, 1, 1, 1
-                    /*7*/, 1, 1, 1, 1, 1, 1, 1, 1
-                    /*8*/, 1, 1, 1, 1, 1, 1, 1, 1
-                   };
+#define notSrclr 6 //shift register clear
+int diodes = sizeof(arr)/sizeof(arr[0]);
 
 void setup() {
   pinMode(ser, OUTPUT);
@@ -28,21 +108,67 @@ void setup() {
   pinMode(srclk, OUTPUT);
   digitalWrite(srclk, LOW);
   digitalWrite(notOE, LOW);
+  Serial.begin(9600);
+  
+  digitalWrite(notSrclr, LOW);
+  delay(5);
+  digitalWrite(notSrclr, HIGH);
+  Serial.println(diodes,DEC);
+
 }
 
 void loop() {
-  LightSeries(arr);
+  //Bake(150);
+  LightAll();
 }
 
-void LightSeries(char arr[])
+void Bake(int timeInSeconds)
 {
-  digitalWrite(rclk, HIGH);
+  LightAll();
+  Serial.println("start");
+  delay(30 * 1000);
+  
+  LightSeries();
+  Serial.println("draw");
+  delay((timeInSeconds - 30)*1000);
+  
+  LightNone();
+  Serial.println("stop");
+  delay(1000*1000);  
+}
+
+void LightAll()
+{
   int i;
-  for (i = 0; i < diodes; i++)
+  for(i=0;i<diodes;i++)
   {
-    LightLED(arr[i]);
+    LightLED(1);
+    digitalWrite(rclk, HIGH);
+    delay(5);
+    digitalWrite(rclk, LOW);
   }
-  digitalWrite(rclk, LOW);
+}
+void LightNone()
+{
+  digitalWrite(6,LOW);
+  delay(5);
+  digitalWrite(6,HIGH);
+}
+
+
+void LightSeries()
+{
+  int i;
+  for(i=0;i<diodes;i++)
+  {
+    Serial.println(arr[i],DEC);
+    LightLED(arr[i]);
+
+    digitalWrite(rclk, HIGH);
+    delay(5);
+    digitalWrite(rclk, LOW);
+  }
+  
 }
 
 void LightLED(bool state)
